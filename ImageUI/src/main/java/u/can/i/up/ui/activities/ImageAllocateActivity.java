f92c159081bd540d62cd2cad857577ab3234de8d
breezedong <dongfeng0511@gmail.com>
2015-06-26 00:12:19 +0800

    修改地图搭配界面

--- ImageUI/src/main/java/u/can/i/up/ui/activities/ImageAllocateActivity.java
+++ ImageUI/src/main/java/u/can/i/up/ui/activities/ImageAllocateActivity.java
@@ -19,6 +19,8 @@ public class ImageAllocateActivity extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_image_allocate);
+
+
     }
 
     @Override
