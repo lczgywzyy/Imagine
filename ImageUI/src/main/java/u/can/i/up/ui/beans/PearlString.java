1ce71b30b3daaba216a6b7e0cbdef3011460ee29
wpp1988 <wxp1988@gmail.com>
2015-07-31 21:25:24 +0800

    pearlString

--- ImageUI/src/main/java/u/can/i/up/ui/beans/PearlString.java
+++ ImageUI/src/main/java/u/can/i/up/ui/beans/PearlString.java
@@ -4,4 +4,10 @@ package u.can.i.up.ui.beans;
  * Created by Pengp on 2015/7/29.
  */
 public class PearlString {
+    private  String names;
+
+
+    public String getNames() {
+        return names;
+    }
 }
